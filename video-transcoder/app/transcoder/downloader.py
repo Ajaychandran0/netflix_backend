d4c0386a2035bdadacf91f83dc502a777a2c8a98
AJAY <ajaychandran2019@gmail.com>
2026-07-31 19:39:42 +0530

    video-transcoder: downloader logic updated

--- video-transcoder/app/transcoder/downloader.py
+++ video-transcoder/app/transcoder/downloader.py
@@ -4,35 +4,50 @@ from app.core.config import static_config, get_s3_client
 from app.core.logger import logger
 
 
-def download_video_from_s3(object_key: str, local_filename: str = None) -> str:
+def download_source_video(
+    object_key: str,
+    local_path: str| Path | None = None
+) -> Path:
     """
     Download a video from S3/MinIO bucket and save it to a local file.
 
     Args:
         object_key (str): The S3 object key (e.g., "uploads/video.mp4").
-        local_filename (str, optional): If not provided, it will be derived from object_key.
+        local_path (str | Path | None, optional): If not provided, it will be derived from object_key.
 
     Returns:
-        str: Path to the local downloaded file.
+        Path: Path to the local downloaded file.
     """
     
     s3 = get_s3_client()
 
-    if not local_filename:
+    if not local_path:
         filename = Path(object_key).name
         local_path = static_config.temp_dir / filename
     else:
-        local_path = Path(local_filename)
+        local_path = Path(local_path)
 
     try:
         local_path.parent.mkdir(parents=True, exist_ok=True)
         
-        logger.info(f"Downloading {object_key} from bucket {static_config.s3_temp_bucket} to {local_path}")
-        
+        logger.info(
+            "Downloading %s from bucket %s to %s",
+            object_key,
+            static_config.s3_temp_bucket,
+            local_path,
+        )        
         s3.download_file(static_config.s3_temp_bucket, object_key, str(local_path))
         
-        logger.info(f"Download completed: {local_path}")
-        return str(local_path)
-    except (BotoCoreError, ClientError) as e:
-        logger.error(f"Failed to download {object_key} from S3: {e}")
+        logger.info(
+            "Download completed: %s",
+            local_path,
+        )
+        
+        return local_path
+    
+    except (BotoCoreError, ClientError):
+        logger.exception(
+            "Failed to download object %s from S3",
+            object_key,
+        )
         raise
